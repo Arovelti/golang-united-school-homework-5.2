80174e0ead34fa5a3ea62ba270eea73a7746af43
d.mikhedko <d.mikhedko@andersenlab.com>
2022-05-22 18:46:13 +0300

    Add solution

--- cache.go
+++ cache.go
@@ -1,23 +1,68 @@
 package cache
 
-import "time"
+import (
+	"sync"
+	"time"
+)
+
+type CacheProperties struct {
+	value    string
+	deadline time.Time
+	elapse   bool
+}
 
 type Cache struct {
+	mu       sync.Mutex
+	cacheMap map[string]CacheProperties
 }
 
 func NewCache() Cache {
-	return Cache{}
+	cache := make(map[string]CacheProperties)
+	return Cache{cacheMap: cache}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c *Cache) Get(key string) (string, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 
+	if k, ok := c.cacheMap[key]; ok {
+		k.elapse = k.deadline.Before(time.Now())
+		if !k.elapse || k.deadline.IsZero() {
+			return k.value, true
+		}
+	}
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.cacheMap[key] = CacheProperties{
+		value:    value,
+		deadline: time.Time{},
+		elapse:   false,
+	}
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	keys := make([]string, 0, len(c.cacheMap))
+	for key, val := range c.cacheMap {
+		val.elapse = val.deadline.Before(time.Now())
+		if !val.elapse || val.deadline.IsZero() {
+			keys = append(keys, key)
+		}
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.cacheMap[key] = CacheProperties{
+		value:    value,
+		deadline: deadline,
+		elapse:   false,
+	}
 }
